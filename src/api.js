f13a24d5a643c12fe1dcc7d19f3910eeb3e6a485
hane-smitter <hanesmitter3@gmail.com>
2021-12-26 01:50:17 +0300

    rewrite functions url

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const request = axios.create({
-  baseURL: "https://us-central1-yt-mimic.cloudfunctions.net/",
-  // baseURL: "/api/",
+  // baseURL: "https://us-central1-yt-mimic.cloudfunctions.net/",
+  baseURL: "/api/",
 });
 
 export default request;
