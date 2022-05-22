24cf10debad2f8de0880a46e62adbb37d8f741be
hane-smitter <hanesmitter3@gmail.com>
2022-05-22 10:59:40 +0300

    fns remove console.log

--- functions/api.js
+++ functions/api.js
@@ -1,8 +1,4 @@
 const axios = require("axios");
-console.log(
-  "ENV youtube_api_key",
-  JSON.stringify(process.env.YOUTUBE_API_KEY, null, 2)
-);
 const request = axios.create({
   baseURL: "https://youtube.googleapis.com/youtube/v3/",
   params: {
